bb57a8361b5d092b5e9d101b8ee80013beb5969a
warp-core <warp-core@users.noreply.github.com>
2025-01-04 22:30:19 +0000

    Update some more include paths

--- source/main.cpp
+++ source/main.cpp
@@ -12,7 +12,7 @@ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 */
 
-#include "Audio.h"
+#include "audio/Audio.h"
 #include "Command.h"
 #include "Conversation.h"
 #include "ConversationPanel.h"
